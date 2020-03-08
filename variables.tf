6b6ce97c01c156a7915f8ae1e1fef12ac4bb35a7
Nikhil Chaudhari <10090206+voldemorte@users.noreply.github.com>
2020-03-08 10:40:07 +0530

    Add pre-commit checks (#3)

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "repositories" {
   description = "A map of repositories"
   // Setting a default for example purpose
-  default     = {
+  default = {
     alpine = {
       options = {
         scan_on_push         = true
         image_tag_mutability = "MUTABLE"
       }
       // For lifecycle rules please set the structure as described
-      // in the selection section of 
+      // in the selection section of
       // https://docs.aws.amazon.com/AmazonECR/latest/userguide/LifecyclePolicies.html
       lifecycle_rules = [
         {
-          tagStatus     = "untagged"
-          // In case of untagged images be sure to not include 
+          tagStatus = "untagged"
+          // In case of untagged images be sure to not include
           // 'tagPrefixList' to avoid failures
-          countType     = "sinceImagePushed"
-          countNumber   = 14
-          countUnit     = "days"
+          countType   = "sinceImagePushed"
+          countNumber = 14
+          countUnit   = "days"
         },
         {
           tagStatus     = "tagged"
@@ -29,15 +29,15 @@ variable "repositories" {
     },
     debian = {
       options = {
-        scan_on_push          = true
-        image_tag_mutability  = "MUTABLE"
+        scan_on_push         = true
+        image_tag_mutability = "MUTABLE"
       }
       lifecycle_rules = [
         {
-          tagStatus     = "untagged"
-          countType     = "sinceImagePushed"
-          countNumber   = 14
-          countUnit     = "days"
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countNumber = 14
+          countUnit   = "days"
         },
         {
           tagStatus     = "tagged"
