56931addea90d3d355437380d98632a5e7e84caa
Nikhil Chaudhari <10090206+voldemorte@users.noreply.github.com>
2020-03-08 15:10:10 +0530

    Set Repository access policy (#4)
    * Set Repository access policy
    
    * Got pre-commit to work

--- main.tf
+++ main.tf
@@ -15,6 +15,47 @@ locals {
       ]
     }
   }
+  repo_access = {
+    for repo, obj in var.repositories : repo => {
+      read_access  = lookup(obj, "read_arns", [])
+      write_access = lookup(obj, "write_arns", [])
+    }
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  for_each = local.repo_access
+
+  statement {
+    sid    = "ECRRead"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = lookup(local.repo_access[each.key], "read_access", [])
+    }
+  }
+  statement {
+    sid    = "ECRPush"
+    effect = "Allow"
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = lookup(local.repo_access[each.key], "write_access", [])
+    }
+  }
 }
 
 resource "aws_ecr_repository" "this" {
@@ -32,4 +73,11 @@ resource "aws_ecr_lifecycle_policy" "this" {
   for_each   = var.repositories
   repository = aws_ecr_repository.this[each.key].name
   policy     = jsonencode(local.lifecycle_policy[each.key])
+}
+
+resource "aws_ecr_repository_policy" "this" {
+  for_each = var.repositories
+
+  repository = aws_ecr_repository.this[each.key].name
+  policy     = data.aws_iam_policy_document.this[each.key].json
 }
